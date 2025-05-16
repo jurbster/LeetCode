c538d260bfb358ac4a796c1eb4a6908e4418bb9b
unknown <jurbster15@yahoo.com>
2025-05-16 15:19:16 -0500

    fix problem 9

--- src/9-palindrone-number.cpp
+++ src/9-palindrone-number.cpp
@@ -5,9 +5,9 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        std::string str1 = std::to_string(x);
-        reverse(str1.begin(), str1.end());
-        int reverseNum = stoi(str1);
+        std::string str1 = std::to_string(x);   //convert x to string
+        reverse(str1.begin(), str1.end());  //reverse string
+        long long reverseNum = stoll(str1); //convert back to integer - gave runtime error with int - too small
 
         if (reverseNum == x){
             return true;
